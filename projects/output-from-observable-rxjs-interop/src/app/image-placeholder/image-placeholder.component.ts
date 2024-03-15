8e993c962b0699cdfb80ad9bf8d64a3488d428a5
Connie Leung <cleung334@gmail.com>
2024-03-15 09:31:08 +0800

    feat: use ouputFromObservable and outputToObservable in the demo

---
+++ projects/output-from-observable-rxjs-interop/src/app/image-placeholder/image-placeholder.component.ts
@@ -0,0 +1,91 @@
+import { ChangeDetectionStrategy, Component, Output, signal } from '@angular/core';
+import { toObservable, outputFromObservable } from '@angular/core/rxjs-interop';
+import { FormsModule } from '@angular/forms';
+import { map, combineLatestWith, debounceTime } from 'rxjs';
+
+@Component({
+  selector: 'app-image-placeholder',
+  standalone: true,
+  imports: [FormsModule],
+  template: `
+    <h3>Redo https://dev.me/products/image-placeholder</h3>
+    <div class="container">
+      <div class="field">
+        <label for="text">
+          <span>Text: </span>
+          <input id="text" name="text" [(ngModel)]="text" />
+        </label>
+      </div>
+      <div class="field">
+        <label for="width">
+          <span>Width: </span>
+          <input id="width" name="width" [(ngModel)]="width" type="number" min="10" />
+        </label>
+      </div>
+      <div class="field">
+        <label for="height">
+          <span>Height: </span>
+          <input id="height" name="height" [(ngModel)]="height" type="number" min="10" />
+        </label>
+      </div>
+      <div class="field">
+        <label for="color">
+          <span>Color: </span>
+          <input id="color" name="color" [(ngModel)]="color" />
+        </label>
+      </div>
+      <div class="field">
+        <label for="backgroundColor">
+          <span>Background color: </span>
+          <input id="backgroundColor" name="backgroundColor" [(ngModel)]="backgroundColor" />
+        </label>
+      </div>
+    </div>
+  `,
+  styles: `
+    h3 {
+      margin-bottom: 1rem;
+    }
+
+    div.container {
+      padding: 0.5rem;
+      border: 1px solid black;
+      border-radius: 12px;
+    }
+
+    div.field {
+      padding: 0.25rem;
+    }
+
+    input {
+      padding: 0.5rem;
+      border: 1px solid #4aa;
+      border-radius: 8px;
+    }
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ImagePlaceholderComponent {
+  text = signal('Signal Output');
+  width = signal(400);
+  height = signal(120);
+  color = signal('#fff');
+  backgroundColor = signal('#000');
+
+  placeholderUrl = outputFromObservable(toObservable(this.text)
+    .pipe(
+      combineLatestWith(toObservable(this.width),
+        toObservable(this.height),
+        toObservable(this.color),
+        toObservable(this.backgroundColor)
+      ),
+      map(([text, width, height, textColor, bgColor]) => {
+        const encodedText = text ? encodeURIComponent(text) : `${width} x ${height}`;
+        const color = encodeURIComponent(textColor);
+        const backgroundColor = encodeURIComponent(bgColor);
+
+        return `https://via.assets.so/img.jpg?w=${width}&h=${height}&&tc=${color}&bg=${backgroundColor}&t=${encodedText}`;
+      }),
+      debounceTime(200)
+    ));
+}
--- projects/output-function/src/app/app.component.ts
+++ projects/output-function/src/app/app.component.ts
@@ -1,13 +1,47 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AsyncPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, OnInit, VERSION, viewChild } from '@angular/core';
+import { ImagePlaceholderComponent } from './image-placeholder/image-placeholder.component';
+import { Observable, scan } from 'rxjs';
+import { outputToObservable } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [ImagePlaceholderComponent, AsyncPipe],
+  template: `
+    <header>Angular {{ version }} - {{ title }}</header>
+    <app-image-placeholder
+      (placeholderUrl)="url = $event"
+    />
+    <p>URL: {{ url }}</p>
+    <p>URL Change {{ urlChangeCount$ | async }} times.</p>
+    <img [src]="url" alt="generic placeholder" />
+  `,
+  styles: `
+    :host {
+      display: block;
+      padding: 0.5rem;
+    }
+
+    img {
+      margin-top: 1rem;
+    }
+
+    p {
+      margin-top: 1rem;
+    }
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-  title = 'output-function';
+export class AppComponent implements OnInit {
+  version = VERSION.full;
+  title = 'outputFromObservable rxJS-interop in 17.3.0';
+  url = '';
+  child = viewChild.required(ImagePlaceholderComponent);
+  urlChangeCount$!: Observable<number>;
+
+  ngOnInit(): void {
+    this.urlChangeCount$ = outputToObservable(this.child().placeholderUrl)
+      .pipe(scan((acc) => acc + 1, 0));
+  }
 }
